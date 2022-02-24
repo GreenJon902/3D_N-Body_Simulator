ee4986045f09603c9981fe619f3de0f0c2f58608
GreenJon902 <greenjon902@gmail.com>
2022-02-24 16:19:01 +0000

    added get distance

--- src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/ThreeD_Utils.java
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/ThreeD_Utils.java
@@ -1,9 +1,17 @@
 package com.greenjon902.cr_squared.threeD_nBody_simulator.renderer;
 
-import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinates;
+import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinate;
 
 public class ThreeD_Utils {
-    public static float getDistance(Coordinates coordinates1, Coordinates coordinates2) {
-       return 1;
+    public static float getDistance(Coordinate coordinate1, Coordinate coordinate2) {
+       float a1 = coordinate2.getX() - coordinate1.getX();
+       float b1 = coordinate2.getY() - coordinate1.getY();
+       float c1 = (float) Math.sqrt(Math.pow(a1, 2) + Math.pow(b1, 2));
+
+       float a2 = c1;
+       float b2 = coordinate2.getZ() - coordinate1.getZ();
+       float c2 = (float) Math.sqrt(Math.pow(a2, 2) + Math.pow(b2, 2));
+
+       return c2;
     }
 }
