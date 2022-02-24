fdea5fec966e67971871c2cc0946e2da76d0f157
GreenJon902 <greenjon902@gmail.com>
2022-02-24 15:59:32 +0000

    added first bit of renderer

--- src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
@@ -1,42 +1,57 @@
 package com.greenjon902.cr_squared.threeD_nBody_simulator.renderer;
 
+import com.greenjon902.cr_squared.threeD_nBody_simulator.Particle;
 import com.greenjon902.cr_squared.threeD_nBody_simulator.ParticleWorld;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Renderer {
-    private final ParticleWorld world;
+    private ParticleWorld particleWorld;
     private final Window window;
     private final Canvas canvas;
 
-    public Renderer(ParticleWorld world) {
-        this(world, new Window() {{
+    public Renderer(ParticleWorld particleWorld) {
+        this(particleWorld, new Window() {{
             setSize(500, 500);
         }});
 
     }
 
-    public Renderer(ParticleWorld world, Window window) {
-        this.world = world;
+    public Renderer(ParticleWorld particleWorld, Window window) {
+        this.particleWorld = particleWorld;
 
         this.window = window;
-        this.window.setVisible(true);
-
-        this.canvas = new Canvas();
+        this.canvas = new Canvas(particleWorld);
         this.window.add(this.canvas);
+        this.window.setVisible(true);
     }
 
     public void mainloop() {
         while (true) {
-
+            canvas.setParticleWorld(particleWorld);
+            window.repaint();
         }
     }
 }
 
 class Canvas extends JPanel {
+    private ParticleWorld particleWorld;
+
+    public Canvas(ParticleWorld particleWorld) {
+        this.particleWorld = particleWorld;
+    }
+
     @Override
-    protected void printComponent(Graphics g) {
-        super.printComponent(g);
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (Particle particle : particleWorld.getParticles()) {
+
+        }
+    }
+
+    public void setParticleWorld(ParticleWorld particleWorld) {
+        this.particleWorld = particleWorld;
     }
 }
