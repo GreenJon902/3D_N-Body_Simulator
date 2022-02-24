ced2bcadd05d7677f0da037fb45ff159064a8c75
GreenJon902 <greenjon902@gmail.com>
2022-02-24 16:19:19 +0000

    get distance of particles

--- src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
@@ -2,9 +2,12 @@ package com.greenjon902.cr_squared.threeD_nBody_simulator.renderer;
 
 import com.greenjon902.cr_squared.threeD_nBody_simulator.Particle;
 import com.greenjon902.cr_squared.threeD_nBody_simulator.ParticleWorld;
+import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinate;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Renderer {
     private ParticleWorld particleWorld;
@@ -46,8 +49,17 @@ class Canvas extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for (Particle particle : particleWorld.getParticles()) {
+        HashMap<Float, Particle> particleDistances = new HashMap<Float, Particle>();
 
+        for (Particle particle : particleWorld.getParticles()) {
+            particleDistances.put(ThreeD_Utils.getDistance(particle.getCoordinates(), new Coordinate(0, 0, 0)), particle);
+        }
+
+        Object[] distances = particleDistances.keySet().toArray();
+        Arrays.sort(distances);
+
+        for (Object distance : distances) {
+            System.out.println(particleDistances.get(distance));
         }
     }
 
