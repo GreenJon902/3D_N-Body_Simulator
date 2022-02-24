64718b305b83ea08218dee06deeaa6f68bd0bf1d
GreenJon902 <greenjon902@gmail.com>
2022-02-24 15:24:16 +0000

    added coordinates class

--- src/test/java/threeD_nBody_simulator/renderer/RendererTest.java
+++ src/test/java/threeD_nBody_simulator/renderer/RendererTest.java
@@ -3,13 +3,14 @@ package threeD_nBody_simulator.renderer;
 import com.greenjon902.cr_squared.threeD_nBody_simulator.Particle;
 import com.greenjon902.cr_squared.threeD_nBody_simulator.ParticleWorld;
 import com.greenjon902.cr_squared.threeD_nBody_simulator.renderer.Renderer;
+import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinates;
 import org.junit.jupiter.api.Test;
 
 public class RendererTest {
     @Test
     public void basicRenderer() {
         ParticleWorld particleWorld = new ParticleWorld();
-        particleWorld.addParticle(new Particle(-1, 10, 0, 0, 0));
+        particleWorld.addParticle(new Particle(new Coordinates(-20, 0, 0), 0, 10, 0, 0, 0));
 
         Renderer renderer = new Renderer(particleWorld);
         renderer.renderLoop();
