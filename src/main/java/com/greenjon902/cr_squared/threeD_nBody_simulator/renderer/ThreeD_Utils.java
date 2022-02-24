58deaef99e17cbc2e585d3940f4bde83766eab61
GreenJon902 <greenjon902@gmail.com>
2022-02-24 15:59:10 +0000

    added function for 3d util

---
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/ThreeD_Utils.java
@@ -0,0 +1,9 @@
+package com.greenjon902.cr_squared.threeD_nBody_simulator.renderer;
+
+import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinates;
+
+public class ThreeD_Utils {
+    public static float getDistance(Coordinates coordinates1, Coordinates coordinates2) {
+       return 1;
+    }
+}
