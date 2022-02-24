73df4072b86a17c8c4d0b389211596805cc2d610
GreenJon902 <greenjon902@gmail.com>
2022-02-24 13:09:33 +0000

    added test

--- src/test/java/threeD_nBody_simulator/renderer/RendererTest.java
+++ src/test/java/threeD_nBody_simulator/renderer/RendererTest.java
@@ -1,6 +1,17 @@
 package threeD_nBody_simulator.renderer;
 
+import com.greenjon902.cr_squared.threeD_nBody_simulator.Particle;
+import com.greenjon902.cr_squared.threeD_nBody_simulator.ParticleWorld;
+import com.greenjon902.cr_squared.threeD_nBody_simulator.renderer.Renderer;
 import org.junit.jupiter.api.Test;
 
 public class RendererTest {
+    @Test
+    public void basicRenderer() {
+        ParticleWorld particleWorld = new ParticleWorld();
+        particleWorld.addParticle(new Particle(-1, 10, 0, 0, 0));
+
+        Renderer renderer = new Renderer(particleWorld);
+        renderer.renderLoop();
+    }
 }
