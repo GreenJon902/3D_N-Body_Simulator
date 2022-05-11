101f7e943536f67e9a386e977c292fc3f6861300
greenjon902 <greenjon902@gmail.com>
2022-05-11 21:24:44 +0100

    added fov to camera

--- src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Camera.java
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Camera.java
@@ -6,13 +6,18 @@ public class Camera {
     private final Coordinate coordinate;
     private final double pitch;
     private final double yaw;
+    private final double fov;
 
-    public Camera(Coordinate coordinate, double pitch, double yaw) {
+    public Camera(Coordinate coordinate, double pitch, double yaw, double fov) {
         this.coordinate = coordinate;
         this.pitch = pitch;
         this.yaw = yaw;
+        this.fov = fov;
     }
 
+    public Camera(Coordinate coordinate, double pitch, double yaw) {
+        this(coordinate, pitch, yaw, 180);
+    }
 
     public Coordinate getCoordinate() {
         return coordinate;
@@ -24,5 +29,9 @@ public class Camera {
 
     public double getYaw() {
         return yaw;
+    }
+
+    public double getFov() {
+        return fov;
     }
 }
