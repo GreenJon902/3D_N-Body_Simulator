572302abc7c5b0f3f144ceb58cf4345f895ede8c
greenjon902 <greenjon902@gmail.com>
2022-05-12 19:01:25 +0100

    added better closing

--- src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
+++ src/main/java/com/greenjon902/cr_squared/threeD_nBody_simulator/renderer/Renderer.java
@@ -6,14 +6,18 @@ import com.greenjon902.cr_squared.threeD_nBody_simulator.structs.Coordinate;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Renderer extends JFrame{
+public class Renderer extends JFrame implements WindowListener {
     private final ParticleWorld particleWorld;
     private final Camera camera;
 
     private final Canvas canvas;
+
+    private boolean mainloop = true;
 
     public Renderer(ParticleWorld particleWorld, Camera camera, int width, int height) {
         this.particleWorld = particleWorld;
@@ -22,6 +26,9 @@ public class Renderer extends JFrame{
         this.setSize(width, height);
         this.canvas = new Canvas(particleWorld, camera);
         this.add(this.canvas);
+
+        this.addWindowListener(this);
+
         this.setVisible(true);
     }
 
@@ -31,13 +38,36 @@ public class Renderer extends JFrame{
 
     public void mainloop() {
         double time = System.currentTimeMillis() / 1000d;
-        while (true) {
+        while (mainloop) {
             double dt = time - System.currentTimeMillis() / 1000d;
             time = System.currentTimeMillis() / 1000d;
             this.repaint();
             camera.setYaw(camera.getYaw() + 100d * dt);
         }
     }
+
+    @Override
+    public void windowOpened(WindowEvent e) {}
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        mainloop = false;
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {}
+
+    @Override
+    public void windowIconified(WindowEvent e) {}
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {}
+
+    @Override
+    public void windowActivated(WindowEvent e) {}
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {}
 }
 
 class Canvas extends JPanel {
