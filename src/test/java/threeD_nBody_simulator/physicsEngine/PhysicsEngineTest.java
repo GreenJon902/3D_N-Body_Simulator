4b665e6199e14bdbc9ec808a047b20fb300e90c4
Callum <Callumr714@icloud.com>
2022-05-26 18:53:24 +0100

    added base values

--- src/test/java/threeD_nBody_simulator/physicsEngine/PhysicsEngineTest.java
+++ src/test/java/threeD_nBody_simulator/physicsEngine/PhysicsEngineTest.java
@@ -3,4 +3,15 @@ package threeD_nBody_simulator.physicsEngine;
 import org.junit.jupiter.api.Test;
 
 public class PhysicsEngineTest {
+    public double G = 6.67428e-11; // newton be like
+    public double AU = 149.6e6; // in km
+
+    private int step = 1; // may or may not be needed
+    private double scale = 250 / AU; // 1 AU = 250 pixels
+
+    @Test
+    public PhysicsEngineTest(double G, double AU){
+
+
+    }
 }
